39fa515abefcdaa8cf2f2478f980ef2e58c0a0f2
Engin Eren <eren@nafhh-cms05.desy.de>
2015-05-13 10:25:41 +0200

    Added comments

---
+++ change_var.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#First argument is parameter name
+#Second argument is parameter value
+#Third argument is parameter uncertainty
+
+#Copy base file which you fixed the parameters
+cp input_steering/minuit.in.txt.Edv.Eg minuit.in.txt
+
+#Change selected parameter
+awk '$2=="'"'$1'"'"{$3='$2'}1' minuit.in.txt > tmp && mv tmp minuit.in.txt
+awk '$2=="'"'$1'"'"{$4='$3'}1' minuit.in.txt > tmp && mv tmp minuit.in.txt
+
+#Uncomment migrad and hesse 
+awk '$1=="call"{$1="*call"}1' minuit.in.txt > tmp && mv tmp minuit.in.txt
+awk '$1=="*migrad"{$1="migrad"}1' minuit.in.txt > tmp && mv tmp minuit.in.txt
+awk '$1=="*hesse"{$1="hesse"}1' minuit.in.txt > tmp && mv tmp minuit.in.txt
+
