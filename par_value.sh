ee3be282d0170a93f362e9ad6d401c577f976932
Engin Eren <Macbookair@Macbook-MacBook-Air.local>
2015-05-14 18:11:38 +0200

    Added comments

--- par_value.sh
+++ par_value.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+#Usage : ./par_value.sh DDbar 
+#$PBS is defined where the output directories are.
+
+#Get the list of output directories which they contain specified variable name (E.g "DUbar")
 LIST=$(ls $PBS | grep "$1")
 
+#Loop and print every specified output directories
 for job in $LIST; do
 	cat $PBS/$job/output/parsout_1 | grep "$1"
 done
 
-
-#cat $PBS/*$1*/output/parsout_1 | grep "$1"
 
 exit 0
 
