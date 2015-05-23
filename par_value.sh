7ce279bd29277b3175284090c8aa6962aa13e31d
Engin Eren <eren@nafhh-cms05.desy.de>
2015-05-23 15:20:36 +0200

    Now calculate the averages of values

--- par_value.sh
+++ par_value.sh
@@ -5,13 +5,23 @@
 
 #Get the list of output directories which they contain specified variable name (E.g "DUbar")
 LIST=$(ls $PBS | grep "$1")
-
+sum_value=0
+sum_value_e=0;
+count=0
 #Loop and print every specified output directories
 for job in $LIST; do
-	echo $job
-	cat $PBS/$job/output/parsout_1 | grep "$1"
+	#echo $job
+	#cat $PBS/$job/output/parsout_1 | grep "$1" 
+	value=$(cat $PBS/$job/output/parsout_1 | grep "$1" | awk '{print $3}') 
+	error=$(cat $PBS/$job/output/parsout_1 | grep "$1" | awk '{print $4}')
+	sum_value=$(awk "BEGIN {print $sum_value+$value; exit}")
+	sum_value_e=$(awk "BEGIN {print $sum_value_e+$error; exit}")
+	count=$((count+1))	
 done
 
-
+#echo $sum_value $count
+average=$(awk "BEGIN {print $sum_value/$count; exit}")
+average_e=$(awk "BEGIN {print $sum_value_e/$count; exit}")
+echo $average $average_e
 exit 0
 
