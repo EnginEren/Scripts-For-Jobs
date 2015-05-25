1ca27bb5053b80b2493c24d75e4a955c3ef95cfd
Engin Eren <eren@nafhh-cms05.desy.de>
2015-05-26 01:11:13 +0200

    Comments are added

--- par_value.sh
+++ par_value.sh
@@ -5,13 +5,15 @@
 
 #Get the list of output directories which they contain specified variable name (E.g "DUbar")
 LIST=$(ls $PBS | grep "$1")
+
+#Define and initialize loop counter and aggregate 
 sum_value=0
 sum_value_e=0;
 count=0
-#Loop and print every specified output directories
+
+#Loop over every specified output directories
+#Sum parameter and error values in the loop
 for job in $LIST; do
-	#echo $job
-	#cat $PBS/$job/output/parsout_1 | grep "$1" 
 	value=$(cat $PBS/$job/output/parsout_1 | grep "$1" | awk '{print $3}') 
 	error=$(cat $PBS/$job/output/parsout_1 | grep "$1" | awk '{print $4}')
 	sum_value=$(awk "BEGIN {print $sum_value+$value; exit}")
@@ -19,7 +21,7 @@ for job in $LIST; do
 	count=$((count+1))	
 done
 
-#echo $sum_value $count
+#Calculate the averages and print
 average=$(awk "BEGIN {print $sum_value/$count; exit}")
 average_e=$(awk "BEGIN {print $sum_value_e/$count; exit}")
 echo $average $average_e
